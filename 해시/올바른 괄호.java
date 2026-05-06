17862b2b97f17ca493356a545fa69c994ace6030
gyudev03 <gyudev03@gmail.com>
2026-05-06 10:07:37 +0900

    Create 올바른 괄호.java

---
+++ 해시/올바른 괄호.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        int cnt = 0;
+        char[] ch = s.toCharArray();
+        
+        for(int i = 0; i<ch.length; i++){
+            if(cnt < 0) answer = false;
+            if(ch[i] == '(') cnt++;
+            else if(ch[i] == ')') cnt--;
+        }
+        if(cnt != 0) answer = false;
+        return answer;
+    }
+}
