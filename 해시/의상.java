6de0a84d239c687795f0729438cc81d9f697a410
gyudev03 <gyudev03@gmail.com>
2026-05-11 21:45:30 +0900

    Update 의상.java

--- 해시/의상.java
+++ 해시/의상.java
@@ -5,9 +5,12 @@ class Solution {
         int answer = 1;
         HashMap<String, Integer> map = new HashMap<>();
         
-        for (String[] c : clothes) map.put(c[1], map.getOrDefault(c[1], 0) + 1);
-        for (int v : map.values()) answer *= v + 1;
-        
+        for (String[] c : clothes){
+            map.put(c[1], map.getOrDefault(c[1], 0) + 1);
+        }
+        for (int v : map.values()){
+            answer *= v + 1;
+        }
         return answer - 1;
     }
 }
